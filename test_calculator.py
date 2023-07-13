f2d405a21a4d169bb9754d1df91e0605782bea33
Annamalai <49229516+Annamalaisaravanan@users.noreply.github.com>
2023-07-13 18:16:45 +0530

    Update test_calculator.py

--- test_calculator.py
+++ test_calculator.py
@@ -2,7 +2,7 @@ import calculator
 
 class Test:
     def test_addition(self):
-           assert 5 == calculator.add(3,3)
+           assert 6 == calculator.add(3,3)
 
     def test_subtraction(self):
             assert -1 == calculator.subtract(2,3)
